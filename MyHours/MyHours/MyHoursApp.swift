e1d74c2b179a9c3d19524288c2ddc93b79e717ac
Sujal Dhungana <sujaldhungana@Sujals-MacBook-Pro.local>
2021-03-16 22:54:45 -0300

    Initial setup

---
+++ MyHours/MyHours/MyHoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHoursApp.swift
+//  MyHours
+//
+//  Created by Sujal Dhungana on 2021-03-16.
+//
+
+import SwiftUI
+
+@main
+struct MyHoursApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
